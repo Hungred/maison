8b30cd3ad968e35f36e2825536e186edf5196408
necrotic7 <necrotic2971@gmail.com>
2021-03-30 16:20:21 +0800

    modify form

--- backend/apps/schedule/urls.py
+++ backend/apps/schedule/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+
+app_name='schedule'
 urlpatterns = [
     path('', views.index),
-    path('sche_modify.html', views.addsche)
+    path('sche_modify/', views.addsche),
 
 ]
