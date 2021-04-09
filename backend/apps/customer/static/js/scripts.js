435dfec76a7dcce758dfa18b92d3e9bf767d125f
roseanita <anita397892@gmail.com>
2021-04-09 17:13:07 +0800

    update customer

---
+++ backend/apps/customer/static/js/scripts.js
@@ -0,0 +1,20 @@
+// Empty JS for your own code to be here
+
+
+var date = new Date();
+
+var nowMonth = date.getMonth() + 1;
+
+var strDate = date.getDate();
+
+var seperator = "-";
+
+if (nowMonth >= 1 && nowMonth <= 9) {
+   nowMonth = "0" + nowMonth;
+}
+
+if (strDate >= 0 && strDate <= 9) {
+   strDate = "0" + strDate;
+}
+
+var nowDate = date.getFullYear() + seperator + nowMonth + seperator + strDate;
