b3fdd8b75e895b91c786fbb8708024ff1e98eb9e
necrotic7 <necrotic2971@gmail.com>
2021-05-07 11:20:33 +0800

    complete register and emp

--- backend/apps/login/models.py
+++ backend/apps/login/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from datetime import date
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Position(models.TextChoices):
+    BOSS = 'B', '老闆'
+    MANAGER = 'M', '管理人員'
+    EMP = 'E', '一般員工'
+
+
+class Employee_data(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    empid = models.CharField('員工編號', max_length=5, unique=True)
+    empname = models.CharField('員工姓名', max_length=4)
+    phonenum = models.CharField('電話號碼', max_length=10)
+    position = models.CharField('職位', max_length=1, choices=Position.choices)
+
+    def __str__(self):
+        field_values = []
+
+        field_values.append(str(self.empid))
+        field_values.append(str(self.empname))
+        return ' '.join(field_values)
+
+
--- backend/apps/login/urls.py
+++ backend/apps/login/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('register', views.sign_up, name='register'),
     path('', views.sign_in, name='index'),
     path('logout', views.log_out, name='logout'),
+    path('change_pwd', views.change_password, name='change'),
+    path('profile/<str:username>', views.profile, name='profile'),
+    path('act_emp', views.active_emp, name='active'),
 ]
