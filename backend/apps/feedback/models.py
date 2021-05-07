98a7daee6e456d7430b4e0d30cca5f8bd8b7738e
OmeletpanM33 <yongzhun@gmail.com>
2021-05-07 15:34:05 +0800

    Revert "Merge remote-tracking branch 'origin/master' into 36"
    This reverts commit 5e926e801eae79acd89f1d35d3fcfba3a2307ea3, reversing
    changes made to b3fdd8b75e895b91c786fbb8708024ff1e98eb9e.

--- backend/apps/feedback/models.py
+++ backend/apps/feedback/models.py
@@ -1,11 +1,3 @@
 from django.db import models
 
-
 # Create your models here.
-class Feedback(models.Model):
-    orderTime = models.DateTimeField('用餐時間')
-    date_submit = models.DateTimeField('提交時間', auto_now=True)
-    feedback_text = models.TextField('反饋内容')
-
-    def __str__(self):
-        return str(self.orderID)
