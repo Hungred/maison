4a4b28e6155d41f98224bbb1fd310c570e616383
roseanita <anita397892@gmail.com>
2021-06-11 17:03:39 +0800

    update customer

--- backend/apps/customer/urls.py
+++ backend/apps/customer/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+
 app_name='customer'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about', views.about, name='about'),
+    path('activity', views.activity, name='activity'),
+    path('environment', views.environment, name='environment'),
+    path('food', views.food, name='food'),
 ]
--- backend/apps/customer/views.py
+++ backend/apps/customer/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import context
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def activity(request):
+    return render(request, 'activity1.html')
+
+def environment(request):
+    return render(request, 'environment.html')
+#
+def food(request):
+    return render(request, 'food.html')
