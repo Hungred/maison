0dded6487b774d875e60f041b12dd2d0856084cf
necrotic7 <necrotic2971@gmail.com>
2021-06-29 20:07:39 +0800

    add filter in views

--- backend/apps/reservation/views.py
+++ backend/apps/reservation/views.py
@@ -3,13 +3,27 @@ from django.http import HttpResponse
 from .models import *
 
 from datetime import date
-
+from django.utils.timezone import datetime, timedelta
 # Create your views here.
 
-
-
 def index(request):
-    reservations = Book.objects.all()
-    context = {'reservations':reservations, 'time':date.today()}
-    return render(request, 'reservation.html', context)
+    today = datetime.today()
+    results = [{
+        'date': (today + timedelta(days=i)).date(),
+        'reservations': []
+    } for i in range(4)]#先產出這四天的日期塞到陣列中
+    recent_reservations = Book.objects\
+        .filter(booktime__range=[today.date(), results[-1]['date']])#從資料庫抓出近四天的預約
+
+    for reservation in recent_reservations:
+        check_day = reservation.booktime.replace(tzinfo=None)#把timezone拿掉
+        index = (check_day - today).days + 1
+        results[index]['reservations'].append(reservation)
+    return render(request, 'reservation.html',
+                  {'recent_reservations': results})
+
+# def index(request):
+#     reservations = Book.objects.all()
+#     context = {'reservations':reservations, 'time':date.today()}
+#     return render(request, 'reservation.html', context)
 
