ba6b919130681126c196a9a88518e066467129a6
necrotic7 <necrotic2971@gmail.com>
2021-10-05 20:42:52 +0800

    fix sche overlap problems

--- backend/apps/schedule/forms.py
+++ backend/apps/schedule/forms.py
@@ -1,6 +1,7 @@
 from django.forms import ModelForm
 from .models import Worksche
 from django import forms
+import datetime as dt
 class WorkscheForm(ModelForm):
     class Meta:
         model = Worksche
@@ -9,7 +10,13 @@ class WorkscheForm(ModelForm):
     def clean(self):
         possible_hour = list(range(1, 25))
         possible_min = list(range(0, 60))
+        date = dt.date.today()
+        print(date)
         workhour = self.cleaned_data.get('workhour')
+        offhour = self.cleaned_data.get('offhour')
+        todaysche = Worksche.objects.filter(workdate=self.cleaned_data.get('workdate')).filter(empid=self.cleaned_data.get('empid'))
+
+
         if int(workhour) not in possible_hour:
             raise forms.ValidationError('非可能的出勤時間')
 
@@ -17,7 +24,7 @@ class WorkscheForm(ModelForm):
         if int(workmin) not in possible_min:
             raise forms.ValidationError('非可能的出勤時間')
 
-        offhour = self.cleaned_data.get('offhour')
+
         if int(offhour) not in possible_hour:
             raise forms.ValidationError('非可能的退勤時間')
 
@@ -27,6 +34,22 @@ class WorkscheForm(ModelForm):
 
         if int(workhour) > int(offhour):
             raise forms.ValidationError('退勤時間必須晚於出勤時間')
+
+        for sche in todaysche:
+            on = sche.workhour
+            off = sche.offhour
+
+            if workhour >= on:
+                if workhour <= off:
+                    raise forms.ValidationError('班表重疊')
+
+            if offhour >= on:
+                if offhour <= off:
+                    raise forms.ValidationError('班表重疊')
+
+            if workhour <= on:
+                if offhour >= off:
+                    raise forms.ValidationError('班表重疊')
         return
 
 
