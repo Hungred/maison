b52f18f4314abd5c75d47223fb3438202e5aca08
necrotic7 <necrotic2971@gmail.com>
2021-11-02 18:09:56 +0800

    delete wt model

--- backend/apps/reservation/models.py
+++ backend/apps/reservation/models.py
@@ -19,9 +19,7 @@ def increment_wait_number():
   return new_wait_id
 
 # Create your models here.
-class WtCheck(models.IntegerChoices):
-    WAITING = 0, '等待中'
-    SERVED = 1, '已帶位'
+
 
 
 class Book(models.Model):
@@ -38,13 +36,3 @@ class Book(models.Model):
         return str(self.booktime)+str(self.bookname)
 
 
-class Wt(models.Model):
-    serno = models.AutoField('流水號', primary_key=True)
-    wid = models.CharField('侯位編號', max_length = 20, default = increment_wait_number, editable=True)
-    wtname = models.CharField('姓', max_length=2)
-    wtadt = models.PositiveIntegerField('大人數量', default=0)
-    wtchd = models.PositiveIntegerField('小孩數量', default=0)
-    wtcheck = models.IntegerField('處理狀態', choices=WtCheck.choices)
-
-    def __str__(self):
-        return str('S')+str(self.serno)+str('/W')+str(self.wid)+str('/')+str(self.wtname)
